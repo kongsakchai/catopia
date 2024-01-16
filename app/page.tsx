f8221ef793b6e663cebca719045b9ce0c081d75c
Atthaset <iaut2545@gmail.com>
2024-01-16 13:49:44 +0700

    fix:login

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ export default function Home() {
       <div className="text-center">
         <input type="text" className="mb-2 p-2 border rounded w-4/5 h-8" />
         <input type="text" className="mb-2 p-2 border rounded w-4/5 h-8" />
-        <small className="block mb-4 self-end">ลืมรหัสผ่าน?</small>
+        <small className="block mb-4 text-primary text-right mr-10">ลืมรหัสผ่าน?</small>
       </div>
-      <button type="submit" className="mt-4 bg-primary text-white p-2 rounded w-4/5 h-8">
-        <span className="">เข้าสู่ระบบ</span>
+      <button type="submit" className="flex mt-4 bg-primary text-white p-2 rounded-md w-4/5 h-8 items-center justify-center">
+        <span>เข้าสู่ระบบ</span>
       </button>
       <div className="text-center mt-4">
         <span>หรือ</span>
         <div className="flex mt-2">
           <span>ยังไม่มีบัญชีใช่ไหม?</span>
-          <p className="ml-2 text-blue-500">ลงทะเบียน</p>
+          <p className="ml-2 text-primary">ลงทะเบียน</p>
         </div>
       </div>
     </div>
