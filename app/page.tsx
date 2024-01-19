e7be5727b555c7fb3ea338f1263a262f4eb0c5d2
Atthaset Niyomsri <94776045+Atthaset@users.noreply.github.com>
2024-01-19 16:40:06 +0700

    feat:Divider line

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,15 @@ export default function Home() {
         </div>
       </Link>
       <div className="text-center mt-4">
-        <span className="text-xs not-italic font-normal leading-5 text-textfield">หรือ</span>
-        <div className="flex mt-4">
-          <span className="text-xs not-italic font-normal leading-5 text-textfield">ยังไม่มีบัญชีใช่ไหม?</span>
-          <Link href="/register" className="text-xs not-italic font-semibold leading-5 ml-1 text-primary">ลงทะเบียน</Link>
+        <div className="flex items-center">
+          <div className="flex-1 w-[320px] h-[0.001px] shrink-0 border border-line"></div>
+          <span className="text-xs not-italic font-normal leading-5 text-textfield px-2">หรือ</span>
+          <div className="flex-1 border border-line"></div>
         </div>
+      </div>
+      <div className="flex mt-4">
+        <span className="text-xs not-italic font-normal leading-5 text-textfield">ยังไม่มีบัญชีใช่ไหม?</span>
+        <Link href="/register" className="text-xs not-italic font-semibold leading-5 ml-1 text-primary">ลงทะเบียน</Link>
       </div>
     </div>
   )
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -12,11 +12,15 @@ export default function Register() {
                 </div>
             </Link>
             <div className="text-center mt-4">
-                <span className="text-xs not-italic font-normal leading-5 text-textfield">หรือ</span>
-                <div className="flex mt-4">
-                    <span className="text-xs not-italic font-normal leading-5 text-textfield">มีบัญชีแล้วใช่ไหม?</span>
-                    <Link href="/" className="text-xs not-italic font-semibold leading-5 ml-1 text-primary">เข้าสู่ระบบ</Link>
+                <div className="flex items-center">
+                    <div className="flex-1 w-[320px] h-[0.001px] shrink-0 border border-line"></div>
+                    <span className="text-xs not-italic font-normal leading-5 text-textfield px-2">หรือ</span>
+                    <div className="flex-1 border border-line"></div>
                 </div>
+            </div>
+            <div className="flex mt-4">
+                <span className="text-xs not-italic font-normal leading-5 text-textfield">มีบัญชีแล้วใช่ไหม?</span>
+                <Link href="/" className="text-xs not-italic font-semibold leading-5 ml-1 text-primary">เข้าสู่ระบบ</Link>
             </div>
         </div>
     )
