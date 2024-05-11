43d2a2f6414ea5a334b80863e8c0b668e515d772
Atthaset Niyomsri <94776045+Atthaset@users.noreply.github.com>
2024-05-11 19:28:13 +0700

    fix: display suggest-home

--- app/component/Homerecommand.tsx
+++ app/component/Homerecommand.tsx
@@ -1,13 +1,30 @@
-import Image from "next/image"
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Homerecommand() {
-    return (
-        <div className="flex flex-col items-start gap-4 w-full border border-solid border-blue-500">
-            <h1 className=" text-black01 text-base font-bold not-italic leading-6">แนะนำ</h1>
-            <div className=" flex flex-col gap-2 w-full">
-                <Image src="/suggest-home-bg.svg" width={364} height={131} alt="suggest-home" className=" rounded-lg"/>
-                <h3 className=" text-black01 text-xs not-italic font-normal leading-5">ระแบบแนะนำแมวที่เหมาะสมกับคุณ</h3>
-            </div>
-        </div>
-    )
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-start gap-4 w-full">
+      <h1 className=" text-black01 text-base font-bold not-italic leading-6">
+        แนะนำ
+      </h1>
+      <div className=" flex flex-col items-center gap-2 w-full">
+        <button type="button" onClick={() => router.push("/main/suggest")}>
+          <Image
+            src="/suggest-home-bg.svg"
+            width={364}
+            height={131}
+            alt="suggest-home"
+            className=" rounded-lg"
+          />
+        </button>
+        <h3 className=" text-black01 text-xs not-italic font-normal leading-5">
+          ระแบบแนะนำแมวที่เหมาะสมกับคุณ
+        </h3>
+      </div>
+    </div>
+  );
 }
